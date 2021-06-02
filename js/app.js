c80b31d25a331ae960c1ba1099cebc54309172db
alexmaga17 <alexmagalhaes17@gmail.com>
2021-06-02 20:50:35 +0100

    app

--- js/app.js
+++ js/app.js
@@ -1,15 +1,67 @@
-import signupView from "./views/signupView.js";
+import userView from "./views/userView.js";
 
 class App {
-	constructor() {
-		this.routes = {
-			"": [],
-			signup: [signupView],
-		};
+    constructor() {
+        // Mapeamento entre os ficheiros HTML e as views que estes vão carregar
+        this.routes = {
+            '': [
+                userView
+            ],
+            'index': [
+                userView
+            ],
+            'login': [
+                userView
+            ],
+            'signup': [
+                userView
+            ]
+        };
 
-		// import dummy data for testing purposes
-		this.#importDataFixtures();
-	}
+        // importa dados dummy para testes
+        this.#importDataFixtures();
+
+        // instancia as views mapeadas no objeto routes
+        this.#instantiateViews();
+    }
+
+    #instantiateViews() {
+        const path = window.location.pathname
+        const file = path.substr(path.lastIndexOf('/') + 1);
+        const route = file.split('.')[0];
+
+        const views = this.#getViews(route);
+
+        for (const view of views) {
+            new view();
+        }
+    }
+
+    #getViews(route) {
+        return typeof this.routes[route] === 'undefined' ? [] : this.routes[route];
+    }
+
+    #importDataFixtures() {
+        const users = [
+            {
+                id: 1,
+                username: 'user1',
+                password: 'pass1'
+            },
+            {
+                id: 2,
+                username: 'user2',
+                password: 'pass2'
+            }
+
+        ];
+
+        // Load the fixtures in case there is no data in the local storage 
+        if (!localStorage.users) {
+            localStorage.setItem('users', JSON.stringify(users));
+        }
+    }
 }
 
 new App();
+
